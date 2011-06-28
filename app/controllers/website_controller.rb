d1bf1e234259c2aa8f9be8b5a8db31ffa8330143
Selim Diaz Araya <selimdiaz@gmail.com>
2011-06-27 19:22:52 -0600

    Initial Version

---
+++ app/controllers/website_controller.rb
@@ -0,0 +1,23 @@
+class WebsiteController < ApplicationController  
+  layout 'website'
+  
+  def servicios
+    if params[:serviceTab]
+      render :inline => "<p><h2>" + params[:serviceTab] + "</h2></p><p>Esta es la información completa de este servicio!!!</p>"
+    end
+  end
+  
+  def index
+  end
+  
+  def about    
+  end
+  
+  def contact    
+  end
+  
+  def location    
+  end
+  
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pacific::Application.config.secret_token = '07e31c179a4c8f8847bdb2563f60269bef9d522259b0616aa86999dc7d5f933b6c0dbb7831703c44810ed805903700c24a661738be062594ba27a4a0d7dfe41a'
