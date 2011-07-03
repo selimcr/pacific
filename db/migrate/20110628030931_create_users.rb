8977d3c16dd7fc95e5ac87ab0da8762ee0fb70a2
Selim Diaz Araya <selimdiaz@gmail.com>
2011-07-03 00:16:14 -0600

    Adding creation of default user

--- db/migrate/20110628030931_create_users.rb
+++ db/migrate/20110628030931_create_users.rb
@@ -14,15 +14,7 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps 
     end 
     
-    @user = User.new
-    @user.login = "admin"
-    @user.name = "Administrator"
-    @user.email = "pacific.seda@gmail.com"
-    @user.role = "Admin"
-    @user.enable = true
-    @user.password = "admin"
-    @user.password_confirmation = "admin"
-    @user.save
+    ActiveRecord::Base.connection.execute("insert into users (login, crypted_password, password_salt, email, persistence_token, single_access_token, perishable_token, role, name, enable, created_at, updated_at) values ('admin', '8759652df6a63358948c143e9226a539b05c7c5afb104d4ba3af52549fc8c7d22aedf06f7efd7275a0e53c4b5feef40d27a1368573a73613a9ca43ab1aa27e2f', 'oKe1p2PE6fNmvUE6gvyA', 'pacific.seda@gmail.com', '308e1ff28ea4d67c0c294f5a26776190a13660bbd4e60a62db5a397b566dbf5b9951160f7a6a40e888c81d546037f44484c43d02949e97f9b8cff2c841a9b2e8', 'j3JjEZVlkAq6OVICASC', 'VCyNushzq8dsxVl06', 'admin', 'Administrador', '1', '2011-06-28 04:07:30', '2011-07-02 22:08:35')")
   end 
   def self.down 
     drop_table :users 
--- db/schema.rb
+++ db/schema.rb
@@ -12,74 +12,6 @@
 
 ActiveRecord::Schema.define(:version => 20110703042812) do
 
-  create_table "activity_log", :id => false, :force => true do |t|
-    t.integer "id",                            :null => false
-    t.integer "trade_id",                      :null => false
-    t.integer "client_id",                     :null => false
-    t.date    "date",                          :null => false
-    t.string  "type",            :limit => 20, :null => false
-    t.string  "currency",        :limit => 10
-    t.float   "exchange_rate",                 :null => false
-    t.float   "amount_invested",               :null => false
-    t.float   "profit"
-    t.float   "percentage",                    :null => false
-  end
-
-  create_table "client", :force => true do |t|
-    t.string  "code",                :limit => 10,                  :null => false
-    t.string  "name",                :limit => 60,                  :null => false
-    t.date    "date",                                               :null => false
-    t.float   "capitalInitial",                    :default => 0.0, :null => false
-    t.float   "capitalActual",                                      :null => false
-    t.float   "capitalInitialMonth",                                :null => false
-    t.integer "level",                                              :null => false
-    t.integer "type",                                               :null => false
-    t.integer "percentage",                                         :null => false
-    t.integer "agent",                                              :null => false
-  end
-
-  create_table "client_account", :force => true do |t|
-    t.integer  "idClient",                   :null => false
-    t.float    "amount",                     :null => false
-    t.string   "description", :limit => 100
-    t.integer  "type",                       :null => false
-    t.datetime "date",                       :null => false
-  end
-
-  create_table "client_close", :force => true do |t|
-    t.integer "client_id",          :null => false
-    t.date    "start_date",         :null => false
-    t.date    "close_date",         :null => false
-    t.float   "capital_on_close",   :null => false
-    t.float   "cap_initial_period", :null => false
-  end
-
-  create_table "client_seller", :id => false, :force => true do |t|
-    t.integer "idClient",   :null => false
-    t.integer "idSeller",   :null => false
-    t.float   "percentage", :null => false
-  end
-
-  add_index "client_seller", ["idClient", "idSeller"], :name => "idClient"
-
-  create_table "conciliation", :force => true do |t|
-    t.float    "amount",  :default => 0.0
-    t.float    "fd",      :default => 0.0, :null => false
-    t.datetime "date",                     :null => false
-    t.float    "beg",     :default => 0.0, :null => false
-    t.float    "pl",      :default => 0.0, :null => false
-    t.float    "roll",    :default => 0.0, :null => false
-    t.float    "nafees",  :default => 0.0, :null => false
-    t.float    "ending",  :default => 0.0, :null => false
-    t.float    "ote",                      :null => false
-    t.float    "liqV",                     :null => false
-    t.float    "mamount",                  :null => false
-    t.float    "mexx",                     :null => false
-    t.float    "sond",                     :null => false
-    t.float    "jent",                     :null => false
-    t.float    "comm",                     :null => false
-  end
-
   create_table "csassociations", :id => false, :force => true do |t|
     t.integer "customer_id"
     t.integer "seller_id"
@@ -101,44 +33,12 @@ ActiveRecord::Schema.define(:version => 20110703042812) do
     t.datetime "updated_at"
   end
 
-  create_table "master", :force => true do |t|
-    t.date   "date",                                      :null => false
-    t.string "type1",      :limit => 20,                  :null => false
-    t.string "type2",      :limit => 20,                  :null => false
-    t.float  "result",                   :default => 0.0, :null => false
-    t.float  "percentage",               :default => 0.0, :null => false
-    t.float  "fill1",                    :default => 0.0, :null => false
-    t.float  "fill2",                    :default => 0.0, :null => false
-  end
-
-  create_table "masterclient", :id => false, :force => true do |t|
-    t.integer "idClient", :null => false
-    t.integer "idTrade",  :null => false
-    t.date    "date",     :null => false
-    t.float   "capital",  :null => false
-    t.integer "level",    :null => false
-    t.float   "income",   :null => false
-  end
-
   create_table "movements", :force => true do |t|
     t.integer  "customer_id"
     t.float    "amount"
     t.text     "description"
     t.integer  "mtype"
     t.datetime "created_at"
-  end
-
-  create_table "profile", :force => true do |t|
-    t.string  "name",    :limit => 100, :null => false
-    t.integer "writer",                 :null => false
-    t.integer "readr",                  :null => false
-    t.integer "searchr",                :null => false
-    t.integer "enable",                 :null => false
-  end
-
-  create_table "seller", :force => true do |t|
-    t.string "name",    :limit => 30, :null => false
-    t.float  "account",               :null => false
   end
 
   create_table "sellers", :force => true do |t|
@@ -157,34 +57,6 @@ ActiveRecord::Schema.define(:version => 20110703042812) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
-  create_table "trade", :id => false, :force => true do |t|
-    t.integer "id",                                             :null => false
-    t.integer "master_id",                                      :null => false
-    t.date    "filldate",                                       :null => false
-    t.string  "type",            :limit => 30,                  :null => false
-    t.string  "ticketBuy",       :limit => 20, :default => "0", :null => false
-    t.string  "ticketSell",      :limit => 20, :default => "0", :null => false
-    t.float   "long",                          :default => 0.0, :null => false
-    t.float   "short",                         :default => 0.0, :null => false
-    t.string  "pair",            :limit => 11,                  :null => false
-    t.float   "fillBuy",                       :default => 0.0, :null => false
-    t.float   "fillSell",                      :default => 0.0, :null => false
-    t.float   "exRt"
-    t.float   "counterCurrBuy",                                 :null => false
-    t.float   "counterCurrSell",                                :null => false
-    t.float   "result",                                         :null => false
-    t.integer "first_trade",     :limit => 1
-  end
-
-  create_table "user", :force => true do |t|
-    t.integer "type",                    :null => false
-    t.string  "name",     :limit => 100, :null => false
-    t.string  "login",    :limit => 30,  :null => false
-    t.string  "password", :limit => 50,  :null => false
-    t.string  "email",    :limit => 100
-    t.integer "enable",                  :null => false
-  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                                 :null => false
